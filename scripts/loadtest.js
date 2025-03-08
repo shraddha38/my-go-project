f52aee837fae00e29be23e1c837ec509d90b502f
Sraddha Sharma <shraddhasharmapro@gmail.com>
2025-03-08 12:33:05 -0600

    chore: refactor

--- scripts/loadtest.js
+++ scripts/loadtest.js
@@ -2,29 +2,29 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 50, // 50 virtual users
-  duration: '10s', // Test duration
+  vus: 10, // 50 virtual users
+  duration: '5s', // Test duration
 };
 
 export default function () {
 
-//   let responseFromRaceCondition = http.get('http://localhost:8080/incrementWithRaceCondition');
-//   // Check if the responses are of status is 200.
-//   check(responseFromRaceCondition, {
-//     'status is 200': (r) => r.status === 200,
-//   });
+  // RACE CONDITION
+  let responseFromRaceCondition = http.get('http://localhost:8080/incrementWithRaceCondition');
+  check(responseFromRaceCondition, {
+    'status is 200': (r) => r.status === 200,
+  });
 
   // PESSIMISTICALLY
-//   let responseFromPessimisticConcurrency = http.get('http://localhost:8080/incrementWithPessimisticLock');
-//   check(responseFromPessimisticConcurrency, {
-//     'status is 200': (r) => r.status === 200,
-//   });
+  // let responseFromPessimisticConcurrency = http.get('http://localhost:8080/incrementWithPessimisticLock');
+  // check(responseFromPessimisticConcurrency, {
+  //   'status is 200': (r) => r.status === 200,
+  // });
 
   // OPTIMISTICALLY
-    let responseFromOptimisticConcurrency = http.get('http://localhost:8080/incrementWithOptimisticVersioning');
-    check(responseFromOptimisticConcurrency, {
-        'status is 200': (r) => r.status === 200,
-    });
+    // let responseFromOptimisticConcurrency = http.get('http://localhost:8080/incrementWithOptimisticVersioning');
+    // check(responseFromOptimisticConcurrency, {
+    //     'status is 200': (r) => r.status === 200,
+    // });
 
 
   sleep(0.01);
